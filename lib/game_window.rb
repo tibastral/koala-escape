61e1ee78d5f9af177f86ebc1faf3ac0b4540811e
Thibaut Assus <thibaut.assus@gmail.com>
2016-03-20 22:46:09 +0100

    Add multiple enemies

--- lib/game_window.rb
+++ lib/game_window.rb
@@ -2,24 +2,27 @@ class GameWindow < Hasu::Window
   WIDTH = 1024
   HEIGHT = 768
   SPEED = 3
+  X_MAX = WIDTH - 128
+  Y_MAX = HEIGHT - 128
 
   def initialize
     super(WIDTH, HEIGHT, false)
+
     @background = Gosu::Image.new(self, 'images/background.png', true)
     @koala = Gosu::Image.new(self, 'images/koala.png', true)
-    @enemy = Gosu::Image.new(self, 'images/enemy.png', true)
+    @enemy_sprite = Gosu::Image.new(self, 'images/enemy.png', true)
     @flag = Gosu::Image.new(self, 'images/flag.png', true)
     @font = Gosu::Font.new(self, Gosu::default_font_name, 30)
     reinit
   end
 
   def reinit
-    @x = 800
-    @y = 600
+    @x = X_MAX
+    @y = Y_MAX
     @finish_area_x = 0
     @finish_area_y = 0
-    @enemy_x = rand(40)
-    @enemy_y = rand(40)
+    @enemies ||= []
+    @enemies << {x: rand(40), y: rand(40)}
   end
 
   def update
@@ -27,7 +30,7 @@ class GameWindow < Hasu::Window
       handle_direction
       handle_win
       handle_loose
-      handle_enemy
+      handle_enemies
     end
     handle_quit
   end
@@ -38,11 +41,13 @@ class GameWindow < Hasu::Window
         @background.draw(x * 128, y * 128, 0)
       end
     end
-    @enemy.draw(@enemy_x, @enemy_y, 2)
+    @enemies.map{|e| @enemy_sprite.draw(e[:x], e[:y], 2)}
     @koala.draw(@x, @y, 2)
     @flag.draw(@finish_area_x, @finish_area_y, 1)
+    @font.draw(@enemies.count, 10, 10, 1, 1.0, 1.0, Gosu::Color::BLACK)
     if @winning
       @font.draw('GAGNÉ !!!', 400, 300, 1.0, 1.0, 1.0, Gosu::Color::BLACK)
+      reinit
     end
     if @loosing
       @font.draw('GAME OVER !!!', 400, 300, 1.0, 1.0, 1.0, Gosu::Color::RED)
@@ -56,6 +61,8 @@ class GameWindow < Hasu::Window
     @x += SPEED if button_down? Gosu::Button::KbRight
     @y -= SPEED if button_down? Gosu::Button::KbUp
     @y += SPEED if button_down? Gosu::Button::KbDown
+    @x = normalize(@x, X_MAX)
+    @y = normalize(@y, Y_MAX)
   end
 
   def handle_win
@@ -63,41 +70,42 @@ class GameWindow < Hasu::Window
   end
 
   def handle_loose
-    @loosing = (@x - @enemy_x).abs < 64 && (@y - @enemy_y).abs < 64
+    @loosing ||= @enemies.any? do |e|
+      (@x - e[:x]).abs < 64 &&
+      (@y - e[:y]).abs < 64
+    end
   end
 
   def random_dir
     rand(3) - 1
   end
 
-  def handle_enemy
-    @for_how_long ||= 0
-    if @for_how_long == 0
-      @x_stay = random_dir
-      @y_stay = random_dir
-      @for_how_long = rand(75)
+  def normalize(v, max)
+    if v < 0
+      0
+    elsif v > max
+      max
+    else
+      v
     end
+  end
 
-    if @for_how_long > 0
-      @for_how_long -= 1
-    end
-
-    puts @for_how_long
-
-    @enemy_x += @x_stay * SPEED
-    @enemy_y += @y_stay * SPEED
-    if @enemy_x < 0
-      @enemy_x = 0
-    end
-    if @enemy_y < 0
-      @enemy_y = 0
-    end
-
-    if @enemy_x > 800
-      @enemy_x = 800
-    end
-    if @enemy_y > 600
-      @enemy_y = 600
+  def handle_enemies
+    @enemies = @enemies.map do |e|
+      e[:for_how_long] ||= 0
+      if e[:for_how_long] == 0
+        e[:x_stay] = random_dir
+        e[:y_stay] = random_dir
+        e[:for_how_long] = rand(75)
+      end
+      if e[:for_how_long] > 0
+        e[:for_how_long] -= 1
+      end
+      e[:x] += e[:x_stay] * SPEED
+      e[:y] += e[:y_stay] * SPEED
+      e[:x] = normalize(e[:x], X_MAX)
+      e[:y] = normalize(e[:y], Y_MAX)
+      e
     end
   end
 
