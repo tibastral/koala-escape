76883f515944c1aefcd7e687b69300cf54f40159
Thibaut Assus <thibaut.assus@gmail.com>
2016-03-21 13:47:03 +0100

    Add multiple enemies

--- lib/game_window.rb
+++ lib/game_window.rb
@@ -1,105 +1,95 @@
 class GameWindow < Hasu::Window
-  WIDTH = 1024
-  HEIGHT = 768
   SPRITE_SIZE = 128
-  X_MAX = WIDTH - SPRITE_SIZE
-  Y_MAX = HEIGHT - SPRITE_SIZE
-  SPRITE_HALF_SIZE = SPRITE_SIZE / 2
+  WINDOW_X = 1024
+  WINDOW_Y = 768
 
   def initialize
-    super(WIDTH, HEIGHT, false)
-
-    @background = Gosu::Image.new(self, 'images/background.png', true)
-    @koala = Gosu::Image.new(self, 'images/koala.png', true)
+    super(WINDOW_X, WINDOW_Y, false)
+    @background_sprite = Gosu::Image.new(self, 'images/background.png', true)
+    @koala_sprite = Gosu::Image.new(self, 'images/koala.png', true)
     @enemy_sprite = Gosu::Image.new(self, 'images/enemy.png', true)
-    @flag = Gosu::Image.new(self, 'images/flag.png', true)
+    @flag_sprite = Gosu::Image.new(self, 'images/flag.png', true)
     @font = Gosu::Font.new(self, Gosu::default_font_name, 30)
+    @flag = {x: WINDOW_X - SPRITE_SIZE, y: WINDOW_Y - SPRITE_SIZE}
+    @music = Gosu::Song.new(self, "musics/koala.wav")
     reset
   end
 
-  def reinit
-    @x = X_MAX
-    @y = Y_MAX
-    @level += 1
-    @speed = @level + 2
-    @finish_area_x = 0
-    @finish_area_y = 0
-    @enemies << {x: rand(40) + WIDTH / 2, y: rand(40) + HEIGHT / 2}
-  end
-
-  def reset
-    @level = 0
-    @enemies = []
-    reinit
-  end
-
   def update
-    handle_direction
+    @player[:x] += @speed if button_down?(Gosu::Button::KbRight)
+    @player[:x] -= @speed if button_down?(Gosu::Button::KbLeft)
+    @player[:y] += @speed if button_down?(Gosu::Button::KbDown)
+    @player[:y] -= @speed if button_down?(Gosu::Button::KbUp)
+    @player[:x] = normalize(@player[:x], WINDOW_X - SPRITE_SIZE)
+    @player[:y] = normalize(@player[:y], WINDOW_Y - SPRITE_SIZE)
     handle_enemies
-    handle_win
-    handle_loose
     handle_quit
-  end
-
-  def handle_win
     if winning?
-      @winning_timer = 50
       reinit
     end
-  end
-
-  def handle_loose
     if loosing?
-      @loosing_timer = 50
       reset
     end
   end
 
   def draw
+    @font.draw("Level #{@enemies.length}", WINDOW_X - 100, 10, 3, 1.0, 1.0, Gosu::Color::BLACK)
+
+    @koala_sprite.draw(@player[:x], @player[:y], 2)
+    @enemies.each do |enemy|
+      @enemy_sprite.draw(enemy[:x], enemy[:y], 2)
+    end
+    @flag_sprite.draw(@flag[:x], @flag[:y], 1)
     (0..8).each do |x|
       (0..8).each do |y|
-        @background.draw(x * 128, y * 128, 0)
+        @background_sprite.draw(x * SPRITE_SIZE, y * SPRITE_SIZE, 0)
       end
-    end
-    @enemies.map{|e| @enemy_sprite.draw(e[:x], e[:y], 2)}
-    @koala.draw(@x, @y, 2)
-    @flag.draw(@finish_area_x, @finish_area_y, 1)
-    @font.draw("Level #{@level}", WIDTH - 100, 10, 1, 1.0, 1.0, Gosu::Color::BLACK)
-    if !@winning_timer.nil? && @winning_timer > 0
-      @winning_timer -= 1
-      @font.draw("LEVEL #{@level} !!", WIDTH / 2 - 30, HEIGHT / 2, 1.0, 1.0, 1.0, Gosu::Color::GREEN)
-    end
-    if !@loosing_timer.nil? && @loosing_timer > 0
-      @loosing_timer -= 1
-      @font.draw('GAME OVER !!!', WIDTH / 2 - 30, HEIGHT / 2, 1.0, 1.0, 1.0, Gosu::Color::RED)
     end
   end
 
   private
 
-  def handle_direction
-    @x -= @speed if button_down? Gosu::Button::KbLeft
-    @x += @speed if button_down? Gosu::Button::KbRight
-    @y -= @speed if button_down? Gosu::Button::KbUp
-    @y += @speed if button_down? Gosu::Button::KbDown
-    @x = normalize(@x, X_MAX)
-    @y = normalize(@y, Y_MAX)
+  def reset
+    @high_score = 0
+    @enemies = []
+    @speed = 3
+    @music.stop
+    @music.play
+    reinit
   end
 
-  def winning?
-    (@x - @finish_area_x).abs < SPRITE_HALF_SIZE &&
-    (@y - @finish_area_y).abs < SPRITE_HALF_SIZE
+  def reinit
+    @speed += 1
+    @player = {x: 0, y: 0}
+    @enemies.push({x: 500 + rand(100), y: 200 + rand(300)})
+    high_score
+  end
+
+  def high_score
+    unless File.exist?('hiscore')
+      File.new('hiscore', 'w')
+    end
+    @new_high_score = [@enemies.count, File.read('hiscore').to_i].max
+    File.write('hiscore', @new_high_score)
+  end
+
+  def collision?(a, b)
+    (a[:x] - b[:x]).abs < SPRITE_SIZE / 2 &&
+    (a[:y] - b[:y]).abs < SPRITE_SIZE / 2
   end
 
   def loosing?
-    @enemies.any? do |e|
-      (@x - e[:x]).abs < SPRITE_HALF_SIZE &&
-      (@y - e[:y]).abs < SPRITE_HALF_SIZE
+    @enemies.any? do |enemy|
+      collision?(@player, enemy)
     end
   end
 
-  def random_dir
-    rand(3) - 1
+  def winning?
+    collision?(@player, @flag)
+  end
+
+  def random_mouvement
+    (rand(3) - 1)
   end
 
   def normalize(v, max)
@@ -112,28 +102,31 @@ class GameWindow < Hasu::Window
     end
   end
 
-  def handle_enemies
-    @enemies = @enemies.map do |e|
-      e[:timer] ||= 0
-      if e[:timer] == 0
-        e[:x_stay] = random_dir
-        e[:y_stay] = random_dir
-        e[:timer] = rand(75)
-      end
-      if e[:timer] > 0
-        e[:timer] -= 1
-      end
-      e[:x] += e[:x_stay] * @speed
-      e[:y] += e[:y_stay] * @speed
-      e[:x] = normalize(e[:x], X_MAX)
-      e[:y] = normalize(e[:y], Y_MAX)
-      e
-    end
-  end
-
   def handle_quit
     if button_down? Gosu::KbEscape
       close
+    end
+  end
+
+  def handle_enemies
+    @enemies = @enemies.map do |enemy|
+      enemy[:timer] ||= 0
+      if enemy[:timer] == 0
+        enemy[:result_x] = random_mouvement
+        enemy[:result_y] = random_mouvement
+        enemy[:timer] = 50 + rand(50)
+      end
+      enemy[:timer] -= 1
+
+      new_enemy = enemy.dup
+      new_enemy[:x] += new_enemy[:result_x] * @speed
+      new_enemy[:y] += new_enemy[:result_y] * @speed
+      new_enemy[:x] = normalize(new_enemy[:x], WINDOW_X - SPRITE_SIZE)
+      new_enemy[:y] = normalize(new_enemy[:y], WINDOW_Y - SPRITE_SIZE)
+      unless collision?(new_enemy, @flag)
+        enemy = new_enemy
+      end
+      enemy
     end
   end
 end
